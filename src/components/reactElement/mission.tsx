fb6b4d6ea975d60f227db95b9539b1e0a9674319
Eliekipata017 <20km245@esisalama.org>
2025-06-03 00:37:35 +0200

    apropos galerie

---
+++ src/components/reactElement/mission.tsx
@@ -0,0 +1,9 @@
+import cameraPhoto from "../../assets/img/cible (1).png"
+export const BodiElementOne = () => {
+    return (
+        <div className={"d-state"}>
+            <img src={cameraPhoto} alt=""/>
+        </div>
+
+    );
+};
